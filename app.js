4b3e39c3fa0df0274ac069099a35f1e4e76413a3
Jett Dembin <71148367+jettdembin@users.noreply.github.com>
2021-03-21 12:09:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ class DrumKit {
         this.bpm = 250;
         this.isPlaying = null;
         this.selects = document.querySelectorAll("select");
+        this.muteBtns = document.querySelectorAll(".mute");
     }
     activePad() {
         this.classList.toggle("active");
@@ -77,6 +78,23 @@ class DrumKit {
                 break;
         }
     }
+    mute(e){
+        const muteIndex = e.target.getAttribute("data-track");
+        e.target.classList.toggle("active");
+        if(e.target.classList.contains('active')){
+            switch(muteIndex){
+                case"0":
+                    this.kickAudio.volume = 0;
+                    break;
+                case"0":
+                    this.snareAudio.volume = 0;
+                    break;
+                case"0":
+                    this.hihatAudio.volume = 0;
+                    break;
+            }
+        }
+    }
 }    
 
 
@@ -101,5 +119,11 @@ drumKit.playBtn.addEventListener("click", function() {
 drumKit.selects.forEach(select => {
     select.addEventListener('change', function(e){
         drumKit.changeSound(e);
+    });
+});
+
+drumKit.muteBtns.forEach(btn => {
+    btn.addEventListener('click', function(e){
+        drumKit.mute(e);
     });
 });
